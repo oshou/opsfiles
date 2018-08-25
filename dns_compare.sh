2b2ccbe368b28d837d0b374b0262ad2be2ed0464
root <root@sakura.(none)>
2018-08-25 10:25:12 +0859

    add sample data

--- dns_compare.sh
+++ dns_compare.sh
@@ -1,27 +1,25 @@
 #!/bin/sh
-# dns compare script
+# compare dig result (only ANSWER SECTION)
 
-readonly DIG_TIMEOUT=3
 
+readonly REQ_TIMEOUT=3
+readonly DOMAIN_LIST="domain_list1.csv domain_list2.csv"
 readonly NAME_SERVERS=(
-  "dns01.jword.jp"
-  "dns02.jword.jp"
-  "ns-a1.cloud.z.com"
-  "ns-a2.cloud.z.com"
-  "ns-a3.cloud.z.com"
+  "test1.example.com"
+  "test2.example.com"
+  "test3.example.com"
+  "test4.example.com"
+  "test5.example.com"
 );
 
-# readonly NAME_SERVERS=(
-#   "dns01.jword.jp"
-#   "dns02.jword.jp"
-# );
 
-cat domain_list.csv | while read -r domain type _; do
+cat $DOMAIN_LIST | while read -r domain type _; do
   val=""
   for name_server in ${NAME_SERVERS[@]}
   do
-    # res=`dig -t $type $domain @$name_server +time=$DIG_TIMEOUT | grep -e "ANSWER SECTION" -e "AUTHORITY SECTION" -A2 | grep -v "ANSWER SECTION"`
-    res=`dig -t $type $domain @$name_server +time=$DIG_TIMEOUT | grep -e "ANSWER SECTION" -A2 | grep -v "ANSWER SECTION"`
+    res=`dig -t $type $domain @$name_server +time=$REQ_TIMEOUT | awk '/ANSWER SECTION/,/(AUTHORITY|ADDITIONAL|Query)/' | head -n -2 | tail -n +2 | awk '{print $1,$3,$4,$5}' | sort`
+    # res=`dig -t $type $domain @$name_server +time=$REQ_TIMEOUT | awk '/;; ANSWER|;; AUTHORITY/,/^$/' | head -n -1 | awk '{print $1,$3,$4,$5}' | sort`
+    # echo $res;
     if [ -z "$val" ]; then
       val="$res"
     fi
